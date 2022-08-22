6a6cc8611fad818ccadd7744c2f19ed1d6949989
umer <umerwell1234@gmail.com>
2022-08-23 00:48:45 +0500

    working

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User();
+        $admin->role_id = 1;
+        $admin->username = 'admin';
+        $admin->email = 'admin@gmail.com';
+        $admin->password = Hash::make(12345678);
+        $admin->phone = 123456789;
+        $admin->save();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\{ Admin };
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('signup',[Admin\AuthController::class,'signup_process']);
+Route::post('login',[Admin\AuthController::class,'login_process']);
+Route::post('reset',[Admin\AuthController::class,'reset_password_process']);
+Route::post('forgot',[Admin\AuthController::class,'forgot_process']);
+Route::middleware('auth:api')->group(function(){
+    Route::get('profile/{id}',[Admin\AuthController::class,'edit_profile']);
+    Route::post('profile/update',[Admin\AuthController::class,'update_profile']);
+    Route::get('logout',[Admin\AuthController::class,'logout']);
+});
