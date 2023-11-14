bbda6164666c5393198e37dc7e0246ce812cbde2
umermuk <umerwell1234@gmail.com>
2023-11-15 04:29:09 +0500

    working

--- app/Http/Requests/Attendance/StoreRequest.php
+++ app/Http/Requests/Attendance/StoreRequest.php
@@ -22,7 +22,7 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'time_in' => 'required',
+            'time_in' => 'nullable',
         ];
     }
 
