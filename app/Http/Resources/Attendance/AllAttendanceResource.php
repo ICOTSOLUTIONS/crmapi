c9db55c85a2a82ea75668cf22ec955cc776f5a40
umermuk <umerwell1234@gmail.com>
2023-11-15 04:41:59 +0500

    working

--- app/Http/Resources/Attendance/AllAttendanceResource.php
+++ app/Http/Resources/Attendance/AllAttendanceResource.php
@@ -19,10 +19,10 @@ class AllAttendanceResource extends JsonResource
         $resource = ((array) $this)['resource']->toArray();
         return [
             'id' => $this->id,
-            'time_in' => ($this->time_in) ? date('Y-m-d h:i:s A' , strtotime($this->time_in)) : '',
-            'time_out' => ($this->time_out) ? date('Y-m-d h:i:s A' , strtotime($this->time_out)) : '',
-            'expected_time_out' => ($this->expected_time_out) ? date('Y-m-d h:i:s A' , strtotime($this->expected_time_out)) : '',
-            'working_time' => ($this->working_time) ? date('Y-m-d H:i:s' , strtotime($this->working_time)) : '',
+            'time_in' => ($this->time_in) ? date('H:i:s A' , strtotime($this->time_in)) : '',
+            'time_out' => ($this->time_out) ? date('H:i:s A' , strtotime($this->time_out)) : '',
+            'expected_time_out' => ($this->expected_time_out) ? date('H:i:s A' , strtotime($this->expected_time_out)) : '',
+            'working_time' => ($this->working_time) ? date('H:i:s' , strtotime($this->working_time)) : '',
             'date' => ($this->date) ? date('Y-m-d' , strtotime($this->date)) : '',
             'status' => $this->status ?? '',
             'employee' => new AllEmployeeResource($this->employee),
