6a1f032e61bbd63b6d4b76795c959d5d7596bc9e
umermuk <umerwell1234@gmail.com>
2023-11-17 03:17:08 +0500

    working

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -209,7 +209,7 @@ class AttendanceController extends Controller
                 // Calculate the difference in minutes
                 $minutesDifference = $time_out->diffInMinutes($time_in);
 
-                $minutesDifference += $breaks * 60;
+                $minutesDifference += $breaks / 60;
                 // Calculate the difference between time in and time out
                 $hoursDifference = $minutesDifference / 60;
 
